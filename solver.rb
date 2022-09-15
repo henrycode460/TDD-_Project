9802c146a5bfd64f267d988720d2c31f0ac9c08f
Henry <dokinruss@gmail.com>
2022-09-15 15:45:19 +0000

    Create the Fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -11,4 +11,18 @@ class Solver
       (1..n).each {|i| ans*=i}
       return ans
     end
+
+    def fizzbuzz(n)
+        
+          if n % 15 == 0
+            "fizzbuzz"
+          elsif n % 3 == 0
+            "fizz"
+        elsif n % 5 == 0 
+            "buzz"
+         else
+           n.to_s
+            
+          end
+    end
 end
