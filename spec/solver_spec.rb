f439e3548d91181e789347dfbc2468b362c3380c
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-09-15 15:27:22 +0000

    Add test for negative and 0 numbers

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,9 +9,19 @@ context "Given a string" do
 end
 
 describe Solver do
-  it "Testing the 'factorial' method" do
-    solve = Solver.new
+  before :each do
+    @solve = Solver.new
+  end
 
-    expect(solve.factorial(4)).to eq(24)
+  it "Testing the 'factorial' method" do
+    expect(@solve.factorial(4)).to eq(24)
+  end
+
+  it "Testing the factorial of 0" do
+    expect(@solve.factorial(0)).to eq(1)
+  end
+
+  it "Testing the factorial of negative number" do
+    expect{@solve.factorial(-4)}.to raise_exception
   end
 end
