12cee07a09168543ca2f21933dda5acd126c132a
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-09-15 15:13:01 +0000

    Write test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,3 +7,11 @@ context "Given a string" do
          expect(words1.reverse_str('hello')).to eq('olleh')
     end
 end
+
+describe Solver do
+  it "Testing the 'factorial' method" do
+    solve = Solver.new
+
+    expect(solve.factorial(4)).to eq(24)
+  end
+end
