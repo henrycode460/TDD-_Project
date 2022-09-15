b1bd44b49856065949d4111a7df5d16283636ee3
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-09-15 15:28:39 +0000

    Modify function to include negative and 0 factoria

--- solver.rb
+++ solver.rb
@@ -7,6 +7,9 @@ class Solver
     end
 
     def factorial(n)
+      if n < 0
+        raise Exception.new "No factorial for negative numbers"
+      end 
       ans = 1
       (1..n).each {|i| ans*=i}
       return ans
